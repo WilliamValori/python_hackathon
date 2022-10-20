0f6f0301339421fe5abccfb127ef143b4979f2aa
William van Eijk <williamvaneijk@gmail.com>
2022-10-20 10:16:41 +0200

    Added dataBase.py

---
+++ test.py
@@ -0,0 +1,4 @@
+import dataBase
+
+dataBase.create_connection("/var/lib/hackathon_db/dataBase.db")
+# dataBase.create_connection("/Users/william/Documents/projects/hackathon_20-10-2022/python_hackathon/db/dataBase.db")
