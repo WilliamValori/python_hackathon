feaee3e09ccfb67f186680c3e8d0238195c10bf1
Michel Lalmohamed <michellalmohamed@valori.com>
2022-10-20 15:39:45 +0200

    Struggle

--- mysite/smoke_generator/models.py
+++ mysite/smoke_generator/models.py
@@ -7,8 +7,5 @@ class Recipe(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name_txt = models.CharField(max_length=200)
     ingredients_txt = models.TextField()
-    image_img = models.ImageField()
+    image_img = models.BinaryField()
     description_txt = models.TextField()
-
-    def __str__(self):
-        return self.title
